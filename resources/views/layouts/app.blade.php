c463509c15e6324423b2eb4baf7d15ffd8d31c98
jmpumero <jmpumero@gmail.com>
2020-08-10 09:28:11 -0400

    vista producto actualizado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -96,7 +96,7 @@
     </div>
 
     <!-- JS, Popper.js, and jQuery -->
-    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <!--datatable jquery-->
 <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -64,33 +64,10 @@
 
    <!--Jquery-->
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <!--datatable jquery-->
 <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
-
-<!--Boostrap-->
-<!-- JS, Popper.js, and jQuery -->
-
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
 
 <script>
